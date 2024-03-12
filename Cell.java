237d032312d95fa04e43c46272c7aa1f3aa183dd
Ryan-T-Orth <135889335+Ryan-T-Orth@users.noreply.github.com>
2024-03-12 12:58:37 -0600

    Challenge 3 attempt 1

---
+++ Cell.java
@@ -0,0 +1,52 @@
+public class Cell {
+
+    private Type type;
+    private Type nextType;
+
+    public Cell() {
+        this.type = this.nextType = Type.UNDECIDED;
+    }
+
+    public Type getType() {
+        return this.type;
+    }
+
+    public void setType(Type t) {
+        this.type = t;
+    }
+
+    public void getInfluence(Cell neighbor, boolean censor) {
+        if (this.type.equals(Type.UNDECIDED)) {this.nextType = neighbor.getType();}
+
+        else if (this.type.equals(Type.TRUTH_LOW)) {
+            if (neighbor.type.equals(Type.UNTRUTH_HI)) {if (!censor) {this.nextType = Type.TRUTH_HI;}}
+        }
+
+        else if (this.type.equals(Type.PARTIAL_A) || this.type.equals(Type.PARTIAL_B)) {
+            if (neighbor.type.equals(Type.UNTRUTH_HI)) {if (!censor) {this.nextType = Type.UNTRUTH_LOW;}}
+            else if (neighbor.type.equals(Type.TRUTH_HI)) {this.nextType = Type.TRUTH_LOW;}
+            else if ((neighbor.type.equals(Type.PARTIAL_A) || neighbor.type.equals(Type.PARTIAL_B)) && !(this.type.equals(neighbor.type))) {
+                this.nextType = Type.TRUTH_LOW;
+            }
+        }
+
+        else if (this.type.equals(Type.UNTRUTH_LOW)) {
+            if (neighbor.type.equals(Type.UNTRUTH_LOW)) {this.nextType = Type.UNTRUTH_HI;}
+            else if (neighbor.type.equals(Type.UNTRUTH_HI)) {this.nextType = Type.UNTRUTH_HI;}
+            else if (neighbor.type.equals(Type.TRUTH_HI)) {this.nextType = Type.TRUTH_LOW;}
+            else if (neighbor.type.equals(Type.PARTIAL_A) || neighbor.type.equals(Type.PARTIAL_B)) {this.nextType = neighbor.type;}
+        }
+
+        else if (this.type.equals(Type.UNTRUTH_HI)) {
+            if (neighbor.type.equals(Type.TRUTH_HI)) {this.nextType = Type.TRUTH_LOW;}
+            else if (neighbor.type.equals(Type.TRUTH_LOW)) {this.nextType = Type.UNTRUTH_LOW;}
+            else if (neighbor.type.equals(Type.PARTIAL_A) || neighbor.type.equals(Type.PARTIAL_B)) {this.nextType = Type.UNTRUTH_LOW;}
+        }
+
+        else {this.nextType = this.type;}
+    }
+
+    public void shift() {
+        this.type = this.nextType;
+    }
+}
---
+++ Simulation.java
@@ -0,0 +1,108 @@
+import java.util.Random;
+
+public class Simulation {
+    public static void main(String[] args) {
+        boolean CENSOR = true;
+        if (args[0].equals("t")) {CENSOR = true;}
+        else if (args[0].equals("f")) {CENSOR = false;}
+        else {System.exit(-1);}
+
+        final int ROWS = 100;
+        final int COLS = 150;
+
+        Cell[][] grid = new Cell[ROWS][COLS];
+
+        for (int i = 0; i < ROWS; i++) {
+            for (int j = 0; j < COLS; j++) {
+                grid[i][j] = new Cell();
+            }
+        }
+
+        Random rand = new Random();
+
+        int others = 6 * ((ROWS * COLS) / 100);
+
+        int randRow;
+        int randCol;
+        
+        for (int i = 0; i < others; i++) {
+            randRow = rand.nextInt(100);
+            randCol = rand.nextInt(150);
+            while ( !(grid[randRow][randCol].getType().equals(Type.UNDECIDED)) ) {
+                randRow = rand.nextInt(100);
+                randCol = rand.nextInt(150);
+            }
+            
+            switch (i % 6) {
+                case 0: 
+                    grid[randRow][randCol].setType(Type.PARTIAL_A);
+                break;
+                case 1: 
+                    grid[randRow][randCol].setType(Type.PARTIAL_B);
+                break;
+                case 2: 
+                    grid[randRow][randCol].setType(Type.TRUTH_LOW);
+                break;
+                case 3: 
+                    grid[randRow][randCol].setType(Type.TRUTH_HI);
+                break;
+                case 4: 
+                    grid[randRow][randCol].setType(Type.UNTRUTH_LOW);
+                break;
+                case 5: 
+                    grid[randRow][randCol].setType(Type.UNTRUTH_HI);
+                break;
+            }
+        }
+
+        for (int n = 0; n < 100; n++) {
+            for (int i = 0; i < ROWS; i++) {
+                for (int j = 0; j < COLS; j++) {
+                    int neighborRow = 0;
+                    int neighborCol = 0;
+
+                    while (neighborRow == 0 && neighborCol == 0) {
+                        if (i == 0) {neighborRow = rand.nextInt(2);} 
+                        else if (i == ROWS) {neighborRow = rand.nextInt(2) - 1;} 
+                        else {neighborRow = rand.nextInt(3) - 1;}
+
+                        if (j == 0) {neighborCol = rand.nextInt(2);}
+                        else if (j == COLS) {neighborCol = rand.nextInt(2) - 1;}
+                        else {neighborCol = rand.nextInt(3) - 1;}
+                    }
+
+                    Cell neighbor = grid[i + neighborRow][j + neighborCol];
+
+                    grid[i][j].getInfluence(neighbor, CENSOR);
+                }
+            }
+
+            for (int i = 0; i < ROWS; i++) {
+                for (int j = 0; j < COLS; j++) {
+                    grid[i][j].shift();
+                }
+            }
+        }
+
+        int truthHi = 0;
+        int truthLow = 0;
+        int untruthHi = 0;
+
+        for (int i = 0; i < ROWS; i++) {
+            for (int j = 0; j < COLS; j++) {
+                if (grid[i][j].getType().equals(Type.TRUTH_HI)) {truthHi++;}
+                if (grid[i][j].getType().equals(Type.TRUTH_LOW)) {truthLow++;}
+                if (grid[i][j].getType().equals(Type.UNTRUTH_HI)) {untruthHi++;}
+            }
+        }
+
+
+        System.out.println("\nMill's Principle of Truth Simulation - 100 Iterations\n");
+        System.out.println("Truth High : " + truthHi);
+        System.out.println("Truth Low : " + truthLow);
+        System.out.println("Truth High + Truth Low : " + (truthHi + truthLow));
+        System.out.println("Untruth High : " + untruthHi + "\n");
+
+
+    }
+}
